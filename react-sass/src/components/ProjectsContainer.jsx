456b6fea5a266478c29e3906ce53549e0f6af951
Victor Lopes <victormoreiralopes@hotmail.com>
2023-03-22 16:35:26 -0300

    ajuste no link do botao de projetos

--- react-sass/src/components/ProjectsContainer.jsx
+++ react-sass/src/components/ProjectsContainer.jsx
@@ -4,7 +4,7 @@ const ProjectsContainer = () => {
     return <section className="projects-container">
         <h2>Projetos</h2>
         <p>Repositorio do Github com todos os meus projetos</p>
-        <a href="#" className="btn">
+        <a href="https://github.com/victorlopesdp13" target="_blank" className="btn">
             Ver projetos
         </a>
     </section>
