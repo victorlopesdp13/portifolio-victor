7cbdbc6525cda59ad36cb4f472bf1f5ddae45101
Victor Lopes <victormoreiralopes@hotmail.com>
2023-03-23 10:50:01 -0300

    linkando o icones para as redes sociais

--- react-sass/src/components/SocialNetworks.jsx
+++ react-sass/src/components/SocialNetworks.jsx
@@ -3,15 +3,15 @@ import {FaLinkedinIn, FaGithub, FaInstagram} from "react-icons/fa";
 import "../styles/components/socialnetworks.sass"
 
 const socialNetworks = [
-    {name: "linkedin", icon: <FaLinkedinIn/> },
-    {name: "github", icon: <FaGithub/> },
-    {name: "Instagram", icon: <FaInstagram/> },
+    {name: "linkedin", href:"https://www.linkedin.com/in/victor-lopes-04a837116/", icon: <FaLinkedinIn/> },
+    {name: "github", href:"https://github.com/victorlopesdp13", icon: <FaGithub/> },
+    
 ];
 
 const SocialNetworks = () => {
     return <section id="social-networks">
         {socialNetworks.map((network) =>(
-           <a href="#" className="social-btn" id={network.name} key={network.name}>
+           <a href={network.href} target="_blank" className="social-btn" id={network.name} key={network.name}>
             {network.icon}
            </a> 
         ))}
