20304b2be7b444cfe1cb94f7b1fe92ed2bf2fb9e
Victor Lopes <victormoreiralopes@hotmail.com>
2023-03-29 15:25:45 -0300

    ajustando comentarios das technologias

--- react-sass/src/components/TechnologiesContainer.jsx
+++ react-sass/src/components/TechnologiesContainer.jsx
@@ -15,14 +15,14 @@ import {
 import "../styles/components/technologiescontainer.sass"
 
 const technologies = [
-    { id:"html", name: "HTML", icon: <DiHtml5 /> },
-    { id:"css", name: "CSS3", icon: <DiCss3 /> },
-    { id:"js", name: "JavaScript", icon: <DiJsBadge /> },
-    { id:"react", name: "React", icon: <DiReact /> },
-    { id:"sass", name: "Sass", icon: <DiSass /> },
-    { id:"bootstrap", name: "Bootstrap", icon: <DiBootstrap /> },
-    { id:"github", name: "Github", icon: <DiGithubBadge /> },
-    { id:"typescript", name: "Typescript", icon: <SiTypescript /> },
+    { id:"html", name: "HTML", text: "Com o HTML aprendi a criar paginas que contenham conteúdos simples, como textos, imagens, vídeos e etc.", icon: <DiHtml5 /> },
+    { id:"css", name: "CSS3", text: "Aprendi que com o css podemos estilizar as páginas web, de forma responsiva com o uso do flex-box e media querys.", icon: <DiCss3 /> },
+    { id:"js", name: "JavaScript", text: "Linguagem de programação responsavel pela interação da pagina com o usuário.", icon: <DiJsBadge /> },
+    { id:"react", name: "React", text: "Biblioteca do javascript com foco em criar interfaces de usuário em paginas web.", icon: <DiReact /> },
+    { id:"sass", name: "Sass", text: "Onde aprendi a escrever css mais limpo, reutilizável e escalável.", icon: <DiSass /> },
+    { id:"bootstrap", name: "Bootstrap", text: "Onde aprendi a melhorar ainda mais minha produtividade no estilos da página.", icon: <DiBootstrap /> },
+    { id:"github", name: "Git e Github", text: "Onde aprendi a contribuir em projetos com uma equipe de forma assincrona.", icon: <DiGithubBadge /> },
+    { id:"typescript", name: "Typescript", text: "Onde aprendi definir o tipo correto de cada dado , manipular dados, criar interfaces entre outras coisas.", icon: <SiTypescript /> },
 ]
 
 const TechnologiesContainer = () => {
@@ -34,7 +34,7 @@ const TechnologiesContainer = () => {
                     {tech.icon}
                     <div className="techlogy-info">
                         <h3>{tech.name}</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p>{tech.text}</p>
                     </div>
                 </div>
            ))} 
